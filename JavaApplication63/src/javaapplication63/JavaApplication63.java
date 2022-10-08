eb2c3a6fd91b46c408312cbc8c35cc2e3bdd793e
JMozar <jhon.moore@unmsm.edu.pe>
2022-10-07 20:41:17 -0500

    tercer commit

--- JavaApplication63/src/javaapplication63/JavaApplication63.java
+++ JavaApplication63/src/javaapplication63/JavaApplication63.java
@@ -6,6 +6,7 @@ public class JavaApplication63 {
 
         public static void main(String[] args) {
             System.out.println("Hola mundo");
+            System.out.println("commit 3");
     }
     
 }

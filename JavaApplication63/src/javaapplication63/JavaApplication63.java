c88bfe1b3989d0a884587d9124a82df3095ee8e7
JMozar <jhon.moore@unmsm.edu.pe>
2022-10-07 20:35:51 -0500

    segundo commit

--- JavaApplication63/src/javaapplication63/JavaApplication63.java
+++ JavaApplication63/src/javaapplication63/JavaApplication63.java
@@ -5,7 +5,7 @@ package javaapplication63;
 public class JavaApplication63 {
 
         public static void main(String[] args) {
-        
+            System.out.println("Hola mundo");
     }
     
 }
